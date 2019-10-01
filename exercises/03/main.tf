439b9987119a5808f143f46102bb10e27241044c
Mark Pevec <mark@tealbook.com>
2019-10-01 14:02:01 -0500

    fixed issue with bucket name

--- exercises/03/main.tf
+++ exercises/03/main.tf
@@ -13,7 +13,7 @@ resource "aws_s3_bucket" "user_student_bucket" {
   bucket  = "rockholla-di-${var.student_alias}"
 }
 resource "aws_s3_bucket_object" "user_student_alias_object" {
-  bucket  = "rockholla-di-${var.student_alias}"
+  bucket  = "${aws_s3_bucket.user_student_bucket.bucket}"
   key     = "student.alias"
   content = "This bucket is reserved for ${var.student_alias}"
 }
